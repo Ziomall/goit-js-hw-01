6473d73a367895320ed87cdc875acf3e6fa213a9
Ziomall <156229643+Ziomall@users.noreply.github.com>
2024-04-19 13:59:29 +0200

    commit

--- task-1.js
+++ task-1.js
@@ -1,7 +1,4 @@
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
-    console.log("You ordered ${quantity} droids worth ${pricePerDroid} credits!: ${totalPrice}");
-  }
-  console.log(makeTransaction(5, 3000));
-  console.log(makeTransaction(3, 1000));
-  console.log(makeTransaction(10, 500));
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
